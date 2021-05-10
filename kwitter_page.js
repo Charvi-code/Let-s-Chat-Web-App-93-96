fa80adfaac11d8b41ae669fb10ffeb70890d14c5
Charvi-code <83527936+Charvi-code@users.noreply.github.com>
2021-05-10 10:10:21 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -13,7 +13,7 @@
     firebase.initializeApp(firebaseConfig);
 
     var user_name=localStorage.getItem("user_name");
-    var room_name=localStorage.getItem("room_name");
+    var room_name=localStorage.getItem("room_name");  
 
     function send()
     {
@@ -36,11 +36,11 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
       name=message_data['name'];
       message=message_data['message'];
       like=message_data['like'];
-      name_with_tag="<h4>"+name+"<img class='user_tick' src='tick.png'></h4>";
+      name_with_tag="<h4> "+name+"<img class='user_tick' src='tick.png'></h4>";
       message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
       like_button="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
-      span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>Like:"+like+"</span></button><hr>";
-      row=name_with_tag+message_with_tag+like_button+span_with_tag;
+      span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>Like:"+ like +"</span></button><hr>";
+      row=name_with_tag+message_with_tag + like_button+span_with_tag;
       document.getElementById("output").innerHTML+=row;
 
 //End code
@@ -49,15 +49,16 @@ getData();
 
 function updateLike(message_id)
 {
-      console.log("clicked on like button- "+message_id);
+      console.log("clicked on like button - "+message_id);
       button_id=message_id;
-      likes = document.getElememtById(button_id).value;
-      updated_likes=Number(likes)+1;
+      likes = document.getElementById(button_id).value;
+      updated_likes=Number(likes) + 1;
       console.log(updated_likes);
+      
       firebase.database().ref(room_name).child(message_id).update({
             like: updated_likes
       });
-}
+}     
 
 function logout()
 {
